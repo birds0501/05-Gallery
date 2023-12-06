5ef0031878e4e9073cd008ef4b58a05b6115ca26
birds0501 <birds0501@naver.com>
2023-12-06 15:01:23 +0900

    code-test브랜치 생성

--- 01-유튜브갤러리/js/script.js
+++ 01-유튜브갤러리/js/script.js
@@ -9,46 +9,52 @@ $(function () {
 
   //이미지를 저장한 배열을 생성
   const imgArr = [
-    "https://file3.instiz.net/data/cached_img/upload/2020/04/15/0/e2c9292403b07385d98ae0727bcd4eed.gif",
-    "https://pbs.twimg.com/media/E1BR9_ZUYAQiV5t.jpg",
-    "https://file3.instiz.net/data/cached_img/upload/2018/09/22/4/a9f915fab8fdb255fe7cfa48d43fe381.gif",
-    "https://mblogthumb-phinf.pstatic.net/MjAxOTA1MjVfMTQg/MDAxNTU4Nzc2OTIzMTIz.YwfNolFIwJ5mv4I6VblYMOhpJpuIhZJzHHEeDg2IPFYg.Dkjg2GOM9-y38yaoFGpLJqgT6UFrpyrJd5_aTfbYBq8g.GIF.leenilee/PleasedClosedDutchsmoushond-size_restricted.gif?type=w800",
+    {
+      title: "[NCT FANCAM WEEK] NCT 127, Regular, Mark Focus [THE SHOW]",
+      video: "https://youtu.be/Cu1rois7Mwo?feature=shared",
+      pic: "https://file3.instiz.net/data/cached_img/upload/2020/04/15/0/e2c9292403b07385d98ae0727bcd4eed.gif",
+    },
+    {
+      title: "NCT U - 'PADO' Archiving Video",
+      video: "https://www.youtube.com/watch?v=6BawyJf4aKs",
+      pic: "https://pbs.twimg.com/media/E1BR9_ZUYAQiV5t.jpg",
+    },
+    {
+      title: "NCT 127 엔시티 127 'Lemonade' Track Video #4",
+      video: "https://youtu.be/ie7IYOYlRkQ?feature=shared",
+      pic: "https://file3.instiz.net/data/cached_img/upload/2018/09/22/4/a9f915fab8fdb255fe7cfa48d43fe381.gif",
+    },
+    {
+      title:
+        "[MPD직캠] 엔시티 127 마크 직캠 4K 'Faster' (NCT 127 MARK FanCam) | @MCOUNTDOWN_2022.9.22",
+      video: "https://youtu.be/MmUNXU77waQ?feature=shared",
+      pic: "https://mblogthumb-phinf.pstatic.net/MjAxOTA1MjVfMTQg/MDAxNTU4Nzc2OTIzMTIz.YwfNolFIwJ5mv4I6VblYMOhpJpuIhZJzHHEeDg2IPFYg.Dkjg2GOM9-y38yaoFGpLJqgT6UFrpyrJd5_aTfbYBq8g.GIF.leenilee/PleasedClosedDutchsmoushond-size_restricted.gif?type=w800",
+    },
   ];
+
+  //접근 테스트
+  console.log(imgArr[0].pic);
+
   //배열에 접근하는 방법은? : 배열[인덱스]
   console.log(imgArr[1]);
-  //body에 배경이미지로 첫번째 이미지 적용
-  // $("body").css("background-image", "url(" + imgArr[0] + ")");
-  // $("body").css("background-image", `url(${imgArr[0]})`);
-  // $selectVideo.css("background-image", `url(${imgArr[0]})`);
-  // console.log($selectVideo, imgArr);
 
-  //li에 이미지를 각각 뿌리자
-  //여러개의 요소에 각각 어떤 것을 해주려 할 때
-  //자바스크립트: for each(function (item) {index})
-  //제이쿼리: each(function (index, item) {})
   $selectVideo.each(function (index, item) {
     console.log(index, item);
-    // $(item).css("background-image", `url(${imgArr[index]})`);
     setBgImage(item, index);
   });
 
   //$selectVideo를 클릭했을 때
   $selectVideo.on("click", function () {
-    //해당 li의 인덱스를 받아서 videoIdx변수에 담아
     const videoIdx = $(this).index();
-    //body에 imgArr배열에서 인덱스에 해당하는 배경이미지를 body에 적용
 
     $("body").css("background-image", `url(${imgArr[videoIdx]})`);
     setBgImage("body", videoIdx);
-    //해당 li의 data-link 값을 가져와서 videoLink 변수에 담자
-    let videoLink = $(this).attr("data-link");
-    videoLink += "?autoplay=1"; //videoLink = videoLink + '?autoplay=1'
-    const videoTitle = $(this).text();
 
-    // console.log(videoLink, videoTitle);
-
-    //그 값을 iframe의 src로 세팅
+    let videoLink = imgArr[videoIdx].video;
+    videoLink += "?autoplay=1";
     $video.attr("src", videoLink);
+
+    const videoTitle = imgArr[videoIdx].title;
 
     //videoTitle을 넣어주기
     $videoWrap.find(".video-title").text(videoTitle);
@@ -57,9 +63,8 @@ $(function () {
     $dim.fadeIn();
     //$videoWrap 보이게 -addClass()
     $videoWrap.addClass("active");
-
-    // console.log($(this));
   });
+
   //닫기 버튼을 클릭했을 때
   $btnClose.on("click", function () {
     $dim.fadeOut();
@@ -67,11 +72,6 @@ $(function () {
     $video.attr("src", "");
     $videoWrap.find(".video-title").text("");
   });
-
-  // $dim.on("click", function () {
-  //   $dim.fadeOut();
-  //   $videoWrap.removeClass("active");
-  // });
 
   $(document).keydown(function (e) {
     if (e.keyCode == 27) {
@@ -82,6 +82,6 @@ $(function () {
 
   //배경 이미지를 적용하는 공통의 동작을 함수로 정의
   function setBgImage(item, index) {
-    $(item).css("background-image", `url(${imgArr[index]})`);
+    $(item).css("background-image", `url(${imgArr[index].pic})`);
   }
 });
